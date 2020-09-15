6da6d1fdc56dabff032d2d6bfa4643b246c85b6d
wangcch <wangcch.cc@gmail.com>
2020-09-15 17:41:19 +0800

    refactor: example func

--- src/demo.ts
+++ src/demo.ts
@@ -1,7 +1,13 @@
 export const demo = "demo";
 
-const asyncFunc = async () => {
-  return "async";
+const asyncFunc = async (): Promise<string> => {
+  const promise = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve("async");
+    }, 300);
+  });
+  const res = await promise;
+  return res;
 };
 
 export default asyncFunc;
